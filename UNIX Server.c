659bcf54f28f471b3a8d3758a76a880f2519751b
VaishnaviThappana <vaishnavi.thappana@gmail.com>
2025-02-25 20:38:35 +0530

    Update UNIX Server.c

--- UNIX Server.c
+++ UNIX Server.c
@@ -1,55 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <unistd.h>
 
-#define SOCKET_PATH "/tmp/unix_socket"
+#define SOCKET_PATH "/tmp/chat_socket"
 
 int main() {
-    int srv_socket, cli_socket;
-    struct sockaddr_un srv_address, cli_address;
-    socklen_t cli_addr_len;
-    char msg_buffer[256];
+    int server_fd, client_fd;
+    struct sockaddr_un addr;
+    char buffer[100];
 
-    srv_socket = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (srv_socket == -1) {
-        perror("Socket creation failed");
+    // Create socket
+    server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("socket");
         exit(EXIT_FAILURE);
     }
 
-    srv_address.sun_family = AF_UNIX;
-    strcpy(srv_address.sun_path, SOCKET_PATH);
-    unlink(SOCKET_PATH); // Remove if the file exists
+    // Set socket address
+    memset(&addr, 0, sizeof(struct sockaddr_un));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, SOCKET_PATH, sizeof(addr.sun_path) - 1);
 
-    if (bind(srv_socket, (struct sockaddr*)&srv_address, sizeof(srv_address)) == -1) {
-        perror("Bind failed");
+    // Bind socket
+    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1) {
+        perror("bind");
         exit(EXIT_FAILURE);
     }
 
-    listen(srv_socket, 5);
-    printf("Server listening on UNIX socket...\n");
-
-    cli_addr_len = sizeof(cli_address);
-    cli_socket = accept(srv_socket, (struct sockaddr*)&cli_address, &cli_addr_len);
-    if (cli_socket == -1) {
-        perror("Accept failed");
+    // Listen for connections
+    if (listen(server_fd, 1) == -1) {
+        perror("listen");
         exit(EXIT_FAILURE);
     }
 
+    printf("Server is waiting for a connection...\n");
+
+    // Accept client connection
+    client_fd = accept(server_fd, NULL, NULL);
+    if (client_fd == -1) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Client connected!\n");
+
+    // Communication loop
     while (1) {
-        memset(msg_buffer, 0, sizeof(msg_buffer));
-        read(cli_socket, msg_buffer, sizeof(msg_buffer));
-        printf("Client: %s\n", msg_buffer);
-        if (strcmp(msg_buffer, "exit") == 0) break;
+        memset(buffer, 0, sizeof(buffer));
+        int n = read(client_fd, buffer, sizeof(buffer) - 1);
+        if (n == 0) {
+            printf("Client disconnected\n");
+            break;
+        } else if (n == -1) {
+            perror("read");
+            break;
+        }
 
+        printf("Client: %s\n", buffer);
+
+        // Send response to client
         printf("Server: ");
-        fgets(msg_buffer, sizeof(msg_buffer), stdin);
-        write(cli_socket, msg_buffer, strlen(msg_buffer));
+        fgets(buffer, sizeof(buffer), stdin);
+        write(client_fd, buffer, strlen(buffer));
     }
 
-    close(cli_socket);
-    close(srv_socket);
+    close(client_fd);
+    close(server_fd);
     unlink(SOCKET_PATH);
     return 0;
 }
